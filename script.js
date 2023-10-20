9afe7c1b5090175d90254f87c8883a9ce74392b1
stephanny <stephann.y@yahoo.com>
2023-10-19 22:32:59 -0300

    AULA 1 JS

---
+++ script.js
@@ -0,0 +1,33 @@
+//let pessoa1 = "stephanny";// Essa variável pode ser atribuida 
+//var pessoa2 = "joão";// Esse tipo de variavel não é mais ultilzado como boas prticas em java script
+//const pessoa3 = "lana";//a variavél do tipo const, só pode ser atribuida uma unica vez
+
+//let mensagem = "Olá, essa é a mansagem da minha variável";
+//console.log(mensagem);
+//mensagem = "Essa é a nova mensagem da minha variável";
+//console.log(mensagem)
+ //float: numeros que contem pontos ou virgula
+ //int: numeros inteiros que não contem numeros ou virgula
+ // bool: booleano: true(v), fals(f)
+// string: do tipo texto, precisa sr colocado entre aspas duplas ou simples
+
+
+//let num2 = 20;
+//console.log(soma);
+//let soma = num1 + num2;
+//let multi = num1 * num2;
+//console.log(multi);
+//let sub = num1 - num2;
+//console.log(sub)
+//let num1 = 15;
+
+//if: se
+//: senão
+// >: maior que
+//<: menor que
+// >=: mior ou igual
+// <=: menor ou igual
+// ==: igual
+
+
+if(um1 >=10) console.log{(" o numero 1 é maior do que 10")}else{ console.log(o numer 1 não é maior do que 10)}
